43e40dc5cd4b53d769f288eccc03d126d7482702
Niimo4 <rimjabal2004@gmail.com>
2023-02-22 15:44:09 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit of a number
- * @n: the int to extract the last digit from
- * Return: value of the last digit
- */
+ * print_last_digit - Prints the last digit of a number
+ *
+ * @n: the number to be treated.
+ *
+ * Return: value of the last digit of number.
+ */ 
 
 int print_last_digit(int n)
 
